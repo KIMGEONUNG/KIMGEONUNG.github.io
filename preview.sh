873023c183b45a3f88b07405dc9a090bd8fa3470
comar <saywooong@gmail.com>
2022-07-12 16:35:02 +0900

    update: trivial

--- preview.sh
+++ preview.sh
@@ -3,7 +3,6 @@
 rm -rf ./_site
 bundle exec jekyll serve
 
-
 # Reference
 # https://jekyllrb.com/docs/
 # https://blog.naver.com/PostView.nhn?blogId=flowerdances&logNo=221110593847&categoryNo=0&parentCategoryNo=0
